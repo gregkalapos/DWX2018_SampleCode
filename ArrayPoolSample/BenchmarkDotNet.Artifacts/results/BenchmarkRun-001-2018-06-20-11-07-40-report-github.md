4fca071ad88852d5e28d48caa4fc4bcd71901ded
Gergely Kalapos <gergo@kalapos.net>
2018-06-20 23:59:35 +0200

    Adding arraypool sample

---
+++ ArrayPoolSample/BenchmarkDotNet.Artifacts/results/BenchmarkRun-001-2018-06-20-11-07-40-report-github.md
@@ -0,0 +1,10 @@
+``` ini
+
+BenchmarkDotNet=v0.10.14, OS=Windows 10.0.15063.1088 (1703/CreatorsUpdate/Redstone2)
+Intel Core i7-6700 CPU 3.40GHz (Skylake), 1 CPU, 8 logical and 4 physical cores
+Frequency=3328125 Hz, Resolution=300.4695 ns, Timer=TSC
+.NET Core SDK=2.1.300
+  [Host] : .NET Core 2.1.0 (CoreCLR 4.6.26515.07, CoreFX 4.6.26515.06), 64bit RyuJIT
+
+There are no benchmarks found 
+
--- ArrayPoolSample/Program.cs
+++ ArrayPoolSample/Program.cs
@@ -1,13 +1,50 @@
 ﻿using System;
 using System.Buffers;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace ArrayPoolSample
 {
-	class Program
+	[MemoryDiagnoser]
+	public class Program
 	{
 		static void Main(string[] args)
 		{
-			ArrayPool<int> a = ArrayPool<int>.Shared;
+			var summary = BenchmarkRunner.Run<Program>();
+		}
+
+		[Benchmark]
+		public static void ArrayPoolBenchmark()
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				MethodCalledOften_ArrayPool();
+			}
+		}
+
+		[Benchmark]
+		public static void NoPoolBenchmark()
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				MethodCalledOften_NoPooling();
+			}
+		}
+
+		public static void MethodCalledOften_NoPooling()
+		{
+			var arr = new int[256 * 1024];
+			Debug.WriteLine(arr[0].ToString());
+			//use arr.
+		}
+
+		public static void MethodCalledOften_ArrayPool()
+		{
+			var arr = ArrayPool<int>.Shared.Rent(256 * 1024);
+			Debug.WriteLine(arr[0].ToString());
+			//use arr.
+			ArrayPool<int>.Shared.Return(arr);
 		}
 	}
 }
